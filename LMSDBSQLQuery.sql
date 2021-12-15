bbbf8fb6fb35c246cff5f06cdc6f6b2c550d814a
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-16 03:09:19 +0530

    [Saddam]Add: Creates Candidate education detail check Table

--- LMSDBSQLQuery.sql
+++ LMSDBSQLQuery.sql
@@ -67,7 +67,7 @@ CREATE TABLE fellowship_candidates (
 CREATE TABLE candidates_personal_det_check (
   id int identity(1,1) primary key NOT NULL,
   candidate_id  int NOT NULL FOREIGN KEY REFERENCES fellowship_candidates(id),
-  field_name int NOT NULL,
+  field_name varchar(255) NOT NULL,
   is_verified int DEFAULT NULL,
   lastupd_stamp datetime DEFAULT NULL,
   lastupd_user int DEFAULT NULL,
@@ -81,13 +81,13 @@ CREATE TABLE candidate_bank_det(
   id int identity(1,1) primary key NOT NULL,
   candidate_id int NOT NULL FOREIGN KEY REFERENCES fellowship_candidates(id),
   name varchar(100) NOT NULL,
-  account_num int NOT NULL,
+  account_num bigint NOT NULL,
   is_account_num_verified int DEFAULT 0,
   ifsc_code varchar(20) NOT NULL,
   is_ifsc_code_verified int DEFAULT 0,
   pan_number varchar(10) DEFAULT NULL,
   is_pan_number_verified int DEFAULT 0,
-  addhaar_num int NOT NULL,
+  addhaar_num bigint NOT NULL,
   is_addhaar_num_verified int DEFAULT 0,
   creator_stamp datetime DEFAULT NULL,
   creator_user int DEFAULT NULL,
@@ -114,6 +114,19 @@ CREATE TABLE candidate_qualification(
   is_training_duration_month_verified int DEFAULT 0,
   other_training varchar(255) DEFAULT NULL,
   is_other_training_verified int DEFAULT 0,
+  creator_stamp datetime DEFAULT NULL,
+  creator_user int DEFAULT NULL
+)
+--=========================================================
+--6.Creates Candidate education_det_check Table
+--=========================================================
+CREATE TABLE candidates_education_det_check (
+  id int NOT NULL,
+  candidate_id  int NOT NULL,
+  field_name varchar(255) NOT NULL,
+  is_verified int DEFAULT NULL,
+  lastupd_stamp datetime DEFAULT NULL,
+  lastupd_user int DEFAULT NULL,
   creator_stamp datetime DEFAULT NULL,
   creator_user int DEFAULT NULL
 )
