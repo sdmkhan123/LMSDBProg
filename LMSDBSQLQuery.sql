175257f442e83f6ce84d54b8152a8be2e2be295c
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-16 03:04:01 +0530

    [Saddam]Add: Create Candidate qualification Table

--- LMSDBSQLQuery.sql
+++ LMSDBSQLQuery.sql
@@ -92,3 +92,28 @@ CREATE TABLE candidate_bank_det(
   creator_stamp datetime DEFAULT NULL,
   creator_user int DEFAULT NULL,
 )
+--=========================================================
+--5.Creates Candidate qualification Table
+--=========================================================
+CREATE TABLE candidate_qualification(
+  id int IDENTITY (1,1) PRIMARY KEY NOT NULL,
+  candidate_id int NOT NULL FOREIGN KEY REFERENCES fellowship_candidates(id),
+  diploma int DEFAULT 0,
+  degree_name varchar(10) NOT NULL,
+  is_degree_name_verified int DEFAULT 0,
+  employee_decipline varchar(100) NOT NULL,
+  is_employee_decipline_verified int DEFAULT 0,
+  passing_year int NOT NULL,
+  is_passing_year_verified int DEFAULT 0,
+  aggr_per float DEFAULT NULL,
+  final_year_per float DEFAULT NULL,
+  is_final_year_per_verified int DEFAULT 0,
+  training_institute varchar(20) NOT NULL,
+  is_training_institute_verified int DEFAULT 0,
+  training_duration_month int DEFAULT NULL,
+  is_training_duration_month_verified int DEFAULT 0,
+  other_training varchar(255) DEFAULT NULL,
+  is_other_training_verified int DEFAULT 0,
+  creator_stamp datetime DEFAULT NULL,
+  creator_user int DEFAULT NULL
+)
