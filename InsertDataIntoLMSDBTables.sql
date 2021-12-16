c9a59ab63f4b861b0b811fae0207f710225c93d6
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-17 03:31:09 +0530

    [Saddam]Add: Insert data into Fellowship Candidates table

--- InsertDataIntoLMSDBTables.sql
+++ InsertDataIntoLMSDBTables.sql
@@ -1,7 +1,26 @@
 use LMSDataBase
+--=========================================================
+--1.Insert data into Hired Candidates Table
+--=========================================================
 insert into hired_candidates values
 ('Saddam','','Khan','sdm@gmail.com','Noida','M.Tech','2020-10-07',8896898606,560037, 'SoftwareDeveloper', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1),
 ('Hanif','','Md','Hanif@gmail.com','Mumbai','M.Tech','2020-08-07',8896998606,560036, 'Testing', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1),
 ('Rani','','Gupta','Rani@gmail.com','Jharkkhand','B.Tech','2020-09-07',8886898606,20037, 'WebDevloper', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1),
 ('Kalpesh','','Kumar','Kalpesh@gmail.com','Delhi','M.Sc','2020-08-07',7896898606,55003, 'SoftwareDeveloper', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1),
 ('Taran','','Singh','Taran@gmail.com','Bangalore','M.Tech','2019-07-07',9896898606,160037, 'Testing', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1)
+--=========================================================
+--2.Insert data into Fellowship Candidates table
+--=========================================================
+insert into fellowship_candidates values
+('Saddam', null, 'Khan', 'sdm@gmail.com ', 'Latur','M.Tech','2019-12-13', 8956748596,413517, 'Backend',
+'Good', 'Good', 'Good', 'Good', '1999-12-13', 1, 'Dnyanoba', 'Farmer', 7584962547, 300000.00, 'Udgir', 'Udgir',
+'photo_path.jpg', '2019-12-13', 'Good', 1, 1, 1, 'Correct', 'Good', null, 1),
+('Rani', null, 'Gupta', 'Rani@gmail.com ', 'Udgir','B.Tech','2018-11-01', 8999772355, 413517, 'Backend',
+'Good', 'Good', 'Good', 'Good', '1998-04-27', 1, 'Vasant', 'Army', 7584962547, 300000.00, 'Udgir', 'Udgir',
+'photo_path.jpg', '2018-12-13', 'Good', 1, 1, 1, 'Correct', 'Good', null, 1),
+('Taran', 'Gill', 'singh', 'singh@gmail.com ', 'Nanded','M.Tech','2017-10-01', 7875965478, 431715, 'Backend',
+'Good', 'Good', 'Good', 'Good', '1997-06-05', 1, 'Eknath', 'Farmer', 7584962547, 300000.00, 'Nanded', 'Nanded',
+'photo_path.jpg', '2017-12-13', 'Good', 1, 1, 1, 'Correct', 'Good', null, 2),
+('Atul', null, 'Shinde', 'atul@gmail.com ', 'Udgir','M.Sc','2019-10-01', 8965741201, 413517, 'Java', 'Good',
+'Fine', 'Good', 'Good', '1997-06-05', 1, 'Arvind', 'Teacher', 7584962547, 450000.00, 'Udgir', 'Udgir',
+'photo_path.jpg', '2019-12-13', 'Good', 1, 1, 1, 'Correct', 'Good', null, 3)
