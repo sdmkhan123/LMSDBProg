db098713ce6b5cd875adf11c205a757462b38823
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-17 00:23:16 +0530

    [Saddam]Add: Inserted data into Hired Candidates Table

--- InsertDataIntoLMSDBTables.sql
+++ InsertDataIntoLMSDBTables.sql
@@ -1,1 +1,7 @@
 use LMSDataBase
+insert into hired_candidates values
+('Saddam','','Khan','sdm@gmail.com','Noida','M.Tech','2020-10-07',8896898606,560037, 'SoftwareDeveloper', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1),
+('Hanif','','Md','Hanif@gmail.com','Mumbai','M.Tech','2020-08-07',8896998606,560036, 'Testing', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1),
+('Rani','','Gupta','Rani@gmail.com','Jharkkhand','B.Tech','2020-09-07',8886898606,20037, 'WebDevloper', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1),
+('Kalpesh','','Kumar','Kalpesh@gmail.com','Delhi','M.Sc','2020-08-07',7896898606,55003, 'SoftwareDeveloper', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1),
+('Taran','','Singh','Taran@gmail.com','Bangalore','M.Tech','2019-07-07',9896898606,160037, 'Testing', 'Good', 'Good', 'Good', 'Good', 'Ok', '',1)
