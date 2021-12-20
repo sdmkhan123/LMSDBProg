53d49c69bd18cb282c8689670544b71754f50933
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-20 16:10:28 +0530

    [Saddam]Add: Writting a query User Defined Multi-Statement Table-Valued Function

--- FunctionsLMSDBQuery.sql
+++ FunctionsLMSDBQuery.sql
@@ -25,3 +25,29 @@ As
 return (Select * from fellowship_candidates)
 --Now we call the above created function
 Select * from GetCandidatTable()
+--=============================================================
+--User Defined Multi-Statement Table-Valued Function
+--=============================================================
+--Create function for CandidateId,first_name, last_name, mobile_num and permanent_pincode of fellowship_candidates
+Create function GetMulCandidate()
+returns @Candidae Table
+(
+Id int, 
+first_name varchar(100),
+last_name varchar(100),
+mobile_num bigint,
+permanent_pincode int
+)
+As
+begin
+Insert into @Candidae
+Select c.id, c.first_name, c.last_name, c.mobile_num, c.permanent_pincode
+From fellowship_candidates c
+--Now update pincode of first candidate
+update @Candidae
+	set permanent_pincode=25000 
+	where id=1
+return
+end
+--Now call the above created function
+Select * from GetMulCandidate() 
