e226f00cdd2a779618a64b73d1e531be5dbc9daf
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-20 15:25:28 +0530

    [Saddam]Add: Creates User Defined Inline Table-Valued Function

--- FunctionsLMSDBQuery.sql
+++ FunctionsLMSDBQuery.sql
@@ -13,4 +13,15 @@ As
 Begin return (Select @first_name + ' '+ @last_name);
 end
 --Calling the above created function
-Select dbo.GetCandidatFullName(first_name,last_name) as Name, permanent_pincode from fellowship_candidates 
+Select dbo.GetCandidatFullName(first_name,last_name) as Name, permanent_pincode from fellowship_candidates
+
+--=============================================================
+--User Defined Inline Table-Valued Function
+--=============================================================
+--Create function to get Fellowship Candidates table
+Create function GetCandidatTable()
+returns Table
+As
+return (Select * from fellowship_candidates)
+--Now we call the above created function
+Select * from GetCandidatTable()
