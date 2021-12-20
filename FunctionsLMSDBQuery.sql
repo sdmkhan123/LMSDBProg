2aed615ff70f74aea6e4d20338f04d10f3f0d0b1
sdmkhan123 <sdmkhan126410@gmail.com>
2021-12-20 15:11:33 +0530

    [Saddam]Add: User defined scalar function implemented

--- FunctionsLMSDBQuery.sql
+++ FunctionsLMSDBQuery.sql
@@ -1,1 +1,16 @@
 use LMSDataBase
+--=============================================================
+--User Defined Scalar Function
+--=============================================================
+--Create function to get Candidate full name and pincode
+Create function GetCandidatFullName
+(
+@first_name varchar(100),
+@last_name varchar(100)
+)
+returns varchar(201)
+As
+Begin return (Select @first_name + ' '+ @last_name);
+end
+--Calling the above created function
+Select dbo.GetCandidatFullName(first_name,last_name) as Name, permanent_pincode from fellowship_candidates 
